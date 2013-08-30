3434e736cab30304e7a83e22a174b349e9af8c69
Mathew Madhavacheril <mathewsyriac@gmail.com>
2013-08-30 10:57:39 -0400

    New options

--- ksb.py
+++ ksb.py
@@ -5,7 +5,7 @@ import numpy as np
 from math import *
 #import timeit
 import sys, getopt
-
+import cmath
 
 
 
@@ -21,21 +21,27 @@ def storeXY(nx,ny):
         x=0.5+i-(nx)/2.0
         for j in range(0,ny):
             y=0.5+j-(ny)/2.0
-            XX[i,j]=x*x
-            XY[i,j]=x*y
-            YY[i,j]=y*y
+            r=9#sqrt(x*x+y*y)
+            if (r<10):
+                XX[i,j]=x*x
+                XY[i,j]=x*y
+                YY[i,j]=y*y
             
             
     
 
-def getQuad(img, nx, ny): # returns the (unweighted) quadrupole matrix of an (nx x ny) img 
+def getQuad(img, px, nx, ny): # returns the (unweighted) quadrupole matrix of an (nx x ny) img 
     global XX, XY, YY
     
     quad=np.array([[0.0,0.0],[0.0,0.0]])
+    if px:
+        mod=1./12.
+    else:
+        mod=0.
 
     quad[1][0]=(img*XY).sum()
-    quad[0][0]=(img*YY).sum() ##### WARNING: X AND Y HAVE BEEN SWAPPED TO
-    quad[1][1]=(img*XX).sum() ##### ACCOUNT FOR NUMPY BEING (Y,X)
+    quad[0][0]=(img*YY).sum()-mod ##### WARNING: X AND Y HAVE BEEN SWAPPED TO
+    quad[1][1]=(img*XX).sum()-mod ##### ACCOUNT FOR NUMPY BEING (Y,X)
     quad[0][1]=quad[1][0]
     
     return quad
@@ -44,8 +50,8 @@ def polE(quad): # returns the KSB "polarization parameters" defined in KSB Eq 3.
     e=np.array([[0.0],[0.0]])
     q1=quad[0][0]-quad[1][1]
     q2=2.0*quad[1][0]
-    T=(quad[0][0]+quad[1][1]) #/2.
-    #T+=2.*sqrt(np.linalg.det(quad))/2.
+    T=(quad[0][0]+quad[1][1]) /2.
+    T+=2.*sqrt(np.linalg.det(quad))/2.
     e[0]=q1/T
     e[1]=q2/T
     return e
@@ -56,7 +62,7 @@ def main(argv):
 
     #DEFAULTS
     n=1000
-    p=20
+    p=40
     g1=0.02
     g2=-0.02
     el=0.3
@@ -64,16 +70,18 @@ def main(argv):
     plot_progression=False
     update_freq=100
     gal_sigma=2.0
+    pxl=True
+    typ='g'
     
     try:
-        opts, args = getopt.getopt(argv,"PIhn:p:1:2:e:s:u:")
+        opts, args = getopt.getopt(argv,"dPIhn:p:1:2:e:s:u:t:")
     except getopt.GetoptError:
         print "My programmer is stupid, so I don't understand the arguments you passed. Run with -h to see available options."
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'python ksb.py -n <number of galaxies> -p <stamp pixels> -1 <shear component 1> -2 <shear component 2> -e <ellipticity> -s <galaxy sigma in pixels>'
-            print 'Add -P if you want to see a plot of estimated shear as a function of n, -I if you want to see an image of the last galaxy -- don\'t use both together'
+            print 'python ksb.py -n <number of galaxies> -p <stamp pixels> -1 <shear component 1> -2 <shear component 2> -e <ellipticity> -s <galaxy sigma in pixels> -u <update frequency> -t <type of profile, g for gaussian, e for exp, s for sersic>'
+            print 'Add -P if you want to see a plot of estimated shear as a function of n, -I if you want to see an image of the last galaxy -- don\'t use both together. Add -d if you don\'t want to convolve with a pixel.'
             sys.exit()
         elif opt == "-n":
             n=int(arg)
@@ -93,11 +101,15 @@ def main(argv):
             plot_progression=True
         elif opt == "-I":
             show_last_gal=True
+        elif opt == "-d":
+            pxl=False
+        elif opt == "-t":
+            typ=arg.strip()
 
-    estimate(n,p,g1,g2,el,gal_sigma,show_last_gal,plot_progression,update_freq)
+    estimate(n,p,g1,g2,el,gal_sigma,show_last_gal,plot_progression,update_freq,typ,pxl)
 
 
-def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq):
+def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq,typ,pxl):
 
 
     shear=[]
@@ -105,7 +117,7 @@ def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq):
     shear.append(g2)
     stamp_xsize = p
     stamp_ysize = p
-    random_seed = 1738401
+    random_seed = 4218409
     pixel_scale = 1.0
     sky_level = 1.e6
     gal_sigma=s
@@ -117,7 +129,16 @@ def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq):
     print "Galaxy sigma in pixels", s
     print "Ellipticity is", el
     print "Number of galaxies is", n, '\n'
-
+    if pxl:
+        print "Will convolve with pixel"
+    else:
+        print "Will not convolve with pixel"
+    if typ=='g':
+        print "Galaxy type is gaussian"
+    elif typ=='e':
+        print "Galaxy type is exponential"
+    else:
+        print "Galaxy type unrecognized. Sersic not implemented yet."
 
     #gi=(sqrt(1+el)-sqrt(1-el))/(sqrt(1+el)+sqrt(1-el)) #shear due to intrinsic ellipticity
 
@@ -129,8 +150,7 @@ def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq):
 
     
     img = galsim.ImageD(stamp_xsize, stamp_ysize,scale=pixel_scale)
-    profile = galsim.Gaussian(sigma=gal_sigma, flux=gal_flux)
-
+    
     first_in_pair=True
 
     storeXY(stamp_xsize,stamp_ysize)
@@ -152,30 +172,37 @@ def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq):
             first_in_pair = True
 
 
-    
+        if typ=='g':    
+            profile = galsim.Gaussian(sigma=gal_sigma, flux=gal_flux)
+        elif typ=='e':
+            profile = galsim.Exponential(scale_radius=gal_sigma/2., flux=gal_flux)
+        else:
+            print "Unrecognized galaxy type"
+            sys.exit(2)
+            
         
         gal=profile.createSheared(e=el, beta=theta)
-    
-    
+        #gal=profile
+        
         g=sqrt(shear[0]**2.+shear[1]**2.)
         gal.applyLensing(g1=shear[0],g2=shear[1],mu=1./(1.-2.*(g**2.)))
-    
-    
+        
+        if pxl:
+            pix = galsim.Pixel(pixel_scale)
+            final=galsim.Convolve([gal,pix])
+        else:
+            final=gal
+	    
         #psf = galsim.Gaussian(sigma=1.0)
-        pix = galsim.Pixel(pixel_scale)
-        final=galsim.Convolve([gal,pix])
         final.draw(img)
 
-        
-        #myarray=np.array([[0.,1.,0.],[1.,1.,1.],[0.,1.,0.]])
-        #print img.array
-        q = getQuad(img.array,*np.shape(img.array))
+        #myimg=drawEllipse(shear[0],shear[1],stamp_xsize,stamp_ysize,s)
+
+        infarr=img.array
+        q = getQuad(infarr,pxl,*np.shape(infarr))
         E=polE(q)
-        #print img.array
-        #print q
-        #print E
     
-    
+
         if ((i % update_freq) is 0) and (i>0):
             print "Done " + str(i) + " galaxies..."
         
@@ -218,9 +245,38 @@ def estimate(n,p,g1,g2,el,s,show_last_gal,plot_progression,update_freq):
         print "Inferred shear"+str(i+1)+" is", inf, "+/-", err, "off by ", (inf-sh)*100/sh, " %", (inf-sh)/err, " sigma"
 
     if show_last_gal:
-        plt.imshow(img.array, interpolation='nearest')
+        plt.subplot (2,2,1)
+        arr=img.array/img.array.sum()
+        plt.imshow(arr, interpolation='nearest')
+        plt.colorbar()
+        #plt.subplot (2,2,2)
+        #plt.imshow(myimg, interpolation='nearest')
+        #plt.colorbar()
+        #plt.subplot (2,2,3)
+        #plt.imshow((arr-myimg)/(myimg+arr+1e-5), interpolation='nearest')
+        #plt.imshow((arr-myimg)/(myimg+1e-5), interpolation='nearest')
+        #plt.imshow(img.array-img2.array, interpolation='nearest')
+        #plt.colorbar()
+
     if show_last_gal or plot_progression:
         plt.show()
+
+def drawEllipse(s1,s2, nx, ny,sigma):
+    z=s1*2+s2*2*1j
+    phi=-cmath.phase(z)/2.
+    r=abs(z)
+    boa=sqrt((1.-r)/(1.+r))
+    img=np.zeros((nx,ny))
+    a=sqrt(2)*sigma
+    b=boa*a
+    for i in range(0,nx):
+        xp=0.5+i-(nx)/2.0        
+        for j in range(0,ny):
+            yp=0.5+j-(ny)/2.0
+            x=cos(phi)*xp-sin(phi)*yp
+            y=sin(phi)*xp+cos(phi)*yp
+            img[j,i]=exp(-((x*x)/(a*a))-((y*y)/(b*b)))
+    return img/img.sum()
 
 
 if __name__=="__main__":
