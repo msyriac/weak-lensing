1494f47baf87c4fca0d142093815711c7b089ad3
Mathew Syriac <mathewsyriac@gmail.com>
2014-01-15 12:28:09 -0500

    pickling to save time

--- pair_postprocess.py
+++ pair_postprocess.py
@@ -7,6 +7,7 @@ from math import *
 import matplotlib.pyplot as p
 import scipy.linalg as l
 from scipy import *
+import pickle
 
 #import generate_pairs as gen
 
@@ -35,47 +36,60 @@ freq=1000
 x=[]
 w=[]
 
-for fname in filesG[:][:]:
-    fG = open(fname, 'r')
-    fH = open(fname.replace('g','h'),'r')
-              
-    readerG = csv.reader(fG,delimiter=',')
-    readerH = csv.reader(fH,delimiter=',')
-    for rowG, rowH in zip(readerG,readerH):
-        i+=1
-        if (i % freq)==0: print i
-        
-        valsG = [float(r) for r in (rv for rv in rowG if rv!='')]
-        valsH = [float(r) for r in (rv for rv in rowH if rv!='')]
+Pickled=False ##################### change to True after first run on new data to save time
 
-        PG=valsG[0]
-        QG=numpy.array([[valsG[1]],
-                        [valsG[2]]])
-        RG=numpy.array( [ [valsG[3], valsG[4]],
-                          [valsG[4], valsG[5]] ] )
+if not Pickled:
+    for fname in filesG[:][:]:
+        fG = open(fname, 'r')
+        fH = open(fname.replace('g','h'),'r')
+                  
+        readerG = csv.reader(fG,delimiter=',')
+        readerH = csv.reader(fH,delimiter=',')
+        for rowG, rowH in zip(readerG,readerH):
+            i+=1
+            if (i % freq)==0: print i
+            
+            valsG = [float(r) for r in (rv for rv in rowG if rv!='')]
+            valsH = [float(r) for r in (rv for rv in rowH if rv!='')]
+    
+            PG=valsG[0]
+            QG=numpy.array([[valsG[1]],
+                            [valsG[2]]])
+            RG=numpy.array( [ [valsG[3], valsG[4]],
+                              [valsG[4], valsG[5]] ] )
+    
+            Q_normG=(QG/PG)
+    
+            CinvG=((numpy.dot(Q_normG,Q_normG.transpose()))-(RG/PG))
+            Cinv_sumG+=CinvG 
+    
+            PH=valsH[0]
+            QH=numpy.array([[valsH[1]],
+                            [valsH[2]]])
+            RH=numpy.array( [ [valsH[3], valsH[4]],
+                              [valsH[4], valsH[5]] ] )
+    
+            Q_normH=(QH/PH)
+            CinvH=((numpy.dot(Q_normH,Q_normH.transpose()))-(RH/PH))
+            Cinv_sumH+=CinvH
+            x.append( (Q_normG.flat, Q_normH.flat) )
+            
+    
+            
+        fG.close()
+        fH.close()
+            
+    pickle.dump(x,open('x.pickle','wb'))
+    pickle.dump(Cinv_sumG,open('Cinv_sumG.pickle','wb'))
+    pickle.dump(Cinv_sumH,open('Cinv_sumH.pickle','wb'))
+    pickle.dump(i,open('i.pickle','wb'))
 
-        Q_normG=(QG/PG)
 
-        CinvG=((numpy.dot(Q_normG,Q_normG.transpose()))-(RG/PG))
-        Cinv_sumG+=CinvG 
-
-        PH=valsH[0]
-        QH=numpy.array([[valsH[1]],
-                        [valsH[2]]])
-        RH=numpy.array( [ [valsH[3], valsH[4]],
-                          [valsH[4], valsH[5]] ] )
-
-        Q_normH=(QH/PH)
-        CinvH=((numpy.dot(Q_normH,Q_normH.transpose()))-(RH/PH))
-        Cinv_sumH+=CinvH
-        x.append( (Q_normG.flat, Q_normH.flat) )
-        
-
-        
-    fG.close()
-    fH.close()
-        
-
+if Pickled:
+   x=pickle.load(open('x.pickle','rb'))
+   Cinv_sumG=pickle.load(open('Cinv_sumG.pickle','rb'))
+   Cinv_sumH=pickle.load(open('Cinv_sumH.pickle','rb'))
+   i=pickle.load(open('i.pickle','rb'))
 
 Cinv=Cinv_sumG+Cinv_sumH
 CC=l.inv(Cinv)*2*i
