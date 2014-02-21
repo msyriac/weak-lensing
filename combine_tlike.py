f672af13ce9f2ffaf7966eaf47e2768dd96d50b7
Mathew Madhavacheril <mathewsyriac@gmail.com>
2014-02-21 16:13:07 -0500

    K calc

--- combine_tlike.py
+++ combine_tlike.py
@@ -2,19 +2,22 @@
 
 from tabulate_like import *
 import sys,scipy
+import numpy as np
+
 print "starting"
 
 G=Gridder(200)
 
 ##option 1
-root='grids'
-pi=3 ## index of plus st
-mi=4 #3 index of minus st
-zi=0 #index of 0 shear
-st=0.01
+#root='/astro/u/anze/work/mat/wl/'+'grids2'
+#root='grids'
+#pi=3 ## index of plus st
+#mi=4 #3 index of minus st
+#zi=0 #index of 0 shear
+#st=0.01
 
 #option2
-root='grids2'
+root='/astro/u/anze/work/mat/wl/'+'grids2'
 pi=4
 mi=1
 zi=0
@@ -32,7 +35,7 @@ for i,gf in enumerate(G.grid):
     gf[0]/=s
     gf[1]/=s
 
-G.dump_to_file(root+'/combined.pickle')
+#G.dump_to_file(root+'/combined.pickle')
 
 # get fishers, etc.
 d1=(G.grid[pi][0]-G.grid[mi][0])/(2*st)
@@ -45,6 +48,8 @@ P=G.grid[zi][0]
 Pa=(P+P[:,::-1])/2.0
 Pb=(P[::-1,:]+P[::-1,::-1])/2.0
 P=(Pa+Pb)/2.0
+#print P
+#sys.exit()
 
 
 F1=df1a*df1b/(Pa*Pb)*P
@@ -72,6 +77,22 @@ L_i=[df1/P, transpose(df1/P)]
 L_ij=[[df1a*df1b/(Pa*Pb),df1a*transpose(df1b)/(Pa*transpose(Pb))],
       [df1a*transpose(df1b)/(Pa*transpose(Pb)),transpose(df1a*df1b/(Pa*Pb))]]
 
+#N=G+H
+#K1 = \int L,, L, L,
+#K1_1111 = L11 * L1 * L1
+
+b=[0,1]
+K1 = [[[[np.zeros((200,200)) for u in b] for v in b] for w in b] for x in b]
+K2 = [[[[np.zeros((200,200)) for u in b] for v in b] for w in b] for x in b]
+for i,j,k,l in [(u,v,w,x) for u in b for v in b for w in b for x in b]:
+    print "Finding Ks for index ", i, j, k, l
+    K1[i][j][k][l] = (L_ij[i][j]*L_i[k]*L_i[l]).sum()
+    K2[i][j][k][l] = (L_ij[i][j]*L_ij[k][l]).sum()
+
+print K1
+
+#print Fisher.shape, L_i[0].shape, L_ij[0][0].shape
+
 I=TabLike(G.N,P,L_i, L_ij, Fisher)
-cPickle.dump(I,open(root+'/tablike.pickle','w'),-1)
+#cPickle.dump(I,open(root+'/tablike.pickle','w'),-1)
 print "done"
